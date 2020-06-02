15ad2f16bb9191321104798e47bc220266f65bc1
unknown <signorfail@gmail.com>
2020-06-02 10:51:02 +0900

    Initial build

---
+++ Assets/scripts/gameState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "gameState")]
+
+public class gameState : ScriptableObject
+{
+    [TextArea(14,10)]
+    [SerializeField] string storyText;
+    // Serializes gamestate with an array called nextStates
+    [SerializeField] gameState[] nextStates;
+
+    public string GetStateStory() 
+    {
+        return storyText;
+    }
+
+    public gameState[] GetNextStates()
+    {
+        return nextStates;
+    }
+}
---
+++ Assets/scripts/pmAdventure.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class pmAdventure : MonoBehaviour
+{
+    // SerializeField makes available the component in Unity
+    [SerializeField] Text textComponent;
+    [SerializeField] gameState startingState;
+
+    // The variable state gets state information from gameState.cs
+    gameState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        manageState();
+    }
+
+    private void manageState()
+    {
+        var nextStates = state.GetNextStates();
+        for (int i = 0; i < nextStates.Length; i++)
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                state = nextStates[i];
+            }
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+}
